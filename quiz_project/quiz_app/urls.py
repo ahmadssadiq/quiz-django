486d8daaaac2daf8c3cbe1d5c3f9567af79a8eb2
ahmadssadiq <91856553+ahmadssadiq@users.noreply.github.com>
2024-11-18 16:37:08 -0500

    implemented base.html, create_quiz.html, and play_quiz and the logic for quiz and creating quiz within views

--- quiz_project/quiz_app/urls.py
+++ quiz_project/quiz_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import sign_up, sign_in, sign_out
+from .views import sign_up, sign_in, sign_out, create_quiz, play_quiz
 
-urlpatterns = [
+urlpatterns += [
     path('sign_up/', sign_up, name='sign_up'),
     path('sign_in/', sign_in, name='sign_in'),
     path('sign_out/', sign_out, name='sign_out'),
+    path('create_quiz/', create_quiz, name='create_quiz'),
+    path('play_quiz/<int:quiz_id>/', play_quiz, name='play_quiz'),
 ]
--- quiz_project/quiz_app/views.py
+++ quiz_project/quiz_app/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from .models import Question, Answer, Quiz
 from django.contrib.auth.decorators import login_required
 
 
@@ -30,3 +32,27 @@ def sign_in(request):
 def sign_out(request):
     logout(request)
     return redirect('sign_in')
+
+@login_required
+def create_quiz(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        description = request.POST['description']
+        category = request.POST['category']
+        difficulty = request.POST['difficulty']
+        quiz = Quiz.objects.create(
+            title=title,
+            description=description,
+            category=category,
+            difficulty=difficulty,
+            owner=request.user
+        )
+        return redirect('index')
+    return render(request, 'quiz_app/create_quiz.html')
+
+@login_required
+def play_quiz(request, quiz_id):
+    quiz = Quiz.objects.get(id=quiz_id)
+    questions = quiz.questions.all()
+    context = {'quiz': quiz, 'questions': questions}
+    return render(request, 'quiz_app/play_quiz.html', context)
