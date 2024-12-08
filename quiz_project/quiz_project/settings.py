d52c89507910d6777810e167b55e614fafdba409
ahmadssadiq <91856553+ahmadssadiq@users.noreply.github.com>
2024-12-08 01:54:31 -0500

    Include SQLite database in version control

--- quiz_project/quiz_project/settings.py
+++ quiz_project/quiz_project/settings.py
@@ -134,11 +134,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",  # For local development
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
-
-# Heroku production database override
-DATABASE_URL = os.getenv("DATABASE_URL")
-if DATABASE_URL:
-    DATABASES["default"] = dj_database_url.config(default=DATABASE_URL)
